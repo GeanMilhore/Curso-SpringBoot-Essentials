7ca426531b767ee7ca1fbe261d05dc29bddf6c17
GeanMilhore <gean.milhore@gmail.com>
2021-12-17 19:50:52 -0300

    aplicacao servlet rodando com get animes

--- src/main/java/academy/devdojo/springboot2/start/ApplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/start/ApplicationStart.java
@@ -2,8 +2,11 @@ package academy.devdojo.springboot2.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @EnableAutoConfiguration
+@ComponentScan(basePackages = "academy.devdojo.springboot2")
 public class ApplicationStart {
     public static void main(String[] args){
         SpringApplication.run(ApplicationStart.class, args);

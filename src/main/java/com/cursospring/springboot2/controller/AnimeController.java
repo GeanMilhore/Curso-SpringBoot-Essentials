9ef45de8389ab3fd12a4610d2e5ebeabac438e21
GeanMilhore <gean.milhore@gmail.com>
2021-12-20 16:04:22 -0300

    metodo GET pt02

--- src/main/java/com/cursospring/springboot2/controller/AnimeController.java
+++ src/main/java/com/cursospring/springboot2/controller/AnimeController.java
@@ -6,7 +6,10 @@ import com.cursospring.springboot2.service.AnimeService;
 import com.cursospring.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +26,13 @@ public class AnimeController {
 
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return  animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/com/cursospring/springboot2/service/AnimeService.java
+++ src/main/java/com/cursospring/springboot2/service/AnimeService.java
@@ -1,16 +1,27 @@
 package com.cursospring.springboot2.service;
 
 import com.cursospring.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService{
 
+    private List<Anime> animes = List.of(new Anime(1L,"Boku no Hero"), new Anime(2L,"Dragon Ball"));
+
     // private final AnimeRepository animeRepository; --futuro
 
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"Boku no Hero"), new Anime(2L,"Dragon Ball"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found!"));
     }
 }

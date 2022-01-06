6e4b65937074e14cbc710b467828bfb757bc428f
GeanMilhore <gean.milhore@gmail.com>
2022-01-06 03:03:39 -0300

    lidando com validacao de campos especificas

--- src/main/java/com/cursospring/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/com/cursospring/springboot2/requests/AnimePostRequestBody.java
@@ -1,14 +1,17 @@
 package com.cursospring.springboot2.requests;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 
-    // varias validações que podemos fazer em campos com o javax.validation
+    // remover campo abaixo na próxima branch
+    @URL(message = "the URL is not valid")
+    private String url;
+
 }

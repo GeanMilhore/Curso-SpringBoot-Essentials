77fc74b569b8c8ff06ae782ca6dc5f169cecad5f
GeanMilhore <gean.milhore@gmail.com>
2022-01-09 19:23:28 -0300

    fazendo mapeamento pelo exchange em SpringClient.java

--- src/main/java/com/cursospring/springboot2/client/SpringClient.java
+++ src/main/java/com/cursospring/springboot2/client/SpringClient.java
@@ -2,17 +2,35 @@ package com.cursospring.springboot2.client;
 
 import com.cursospring.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
-        //                                                                  podemos passar como placeholder {id} e adicionar o valor depois da classe
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/7", Anime.class);
         log.info(entity);
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/7", Anime.class);
         log.info(object);
+
+        // modo array
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+        log.info(Arrays.toString(animes));
+
+        // supertype token, método exchange
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange(
+                "http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                /* reques object  */ null,
+                new ParameterizedTypeReference<>() {}
+        );
+        log.info(exchange.getBody());
+
     }
 }
--- src/main/java/com/cursospring/springboot2/service/AnimeService.java
+++ src/main/java/com/cursospring/springboot2/service/AnimeService.java
@@ -24,6 +24,8 @@ public class AnimeService{
         return animeRepository.findAll(pageable);
     }
 
+    public List<Anime> listAllNonPageable() { return animeRepository.findAll(); }
+
     public List<Anime> findByName(String name){
         return animeRepository.findByName(name);
     }
@@ -48,4 +50,6 @@ public class AnimeService{
         anime.setId(savedAnime.getId());
         animeRepository.save(anime);
     }
+
+
 }

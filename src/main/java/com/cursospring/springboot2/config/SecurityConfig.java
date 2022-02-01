f755a7a6abcfd95a2d36ec81b75570b4da5df50f
GeanMilhore <gean.milhore@gmail.com>
2022-02-01 17:45:12 -0300

    spring security authorizatino e usuario em mÃemória

---
+++ src/main/java/com/cursospring/springboot2/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.cursospring.springboot2.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoded {}", passwordEncoder.encode("test"));
+
+        // Usuário em memória duranto o ciclo de vida da aplicação
+        auth.inMemoryAuthentication()
+                .withUser("gean")
+                .password(passwordEncoder.encode("senha"))
+                .roles("USER","ADMIN")
+                .and()
+                .withUser("usuario")
+                .password(passwordEncoder.encode("senha"))
+                .roles("USER");
+    }
+}

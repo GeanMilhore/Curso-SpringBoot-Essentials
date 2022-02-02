f5e122f2334c3509e7d374433043b7e3ff07f4ca
GeanMilhore <gean.milhore@gmail.com>
2022-02-02 16:36:24 -0300

    autorizacao no controller de acordo com a role

--- src/main/java/com/cursospring/springboot2/config/SecurityConfig.java
+++ src/main/java/com/cursospring/springboot2/config/SecurityConfig.java
@@ -2,15 +2,16 @@ package com.cursospring.springboot2.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -18,31 +19,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf()
                 .disable()
-
-                // http only = true /  means that front end software cannot get cookie
-//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-
-                // XSRF-TOKEN appears in cookies
-
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
-
-
-        //                      CÓDIGO NO POSTMAN
-
-        //        var xsrfCookie = postman.getResponseCookie("XSRF-TOKEN");
-        //        postman.setEnvironmentVariable("x-xsrf-token", xsrfCookie.value);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        log.info("Password encoded {}", passwordEncoder.encode("test"));
-
-        // Usuário em memória duranto o ciclo de vida da aplicação
         auth.inMemoryAuthentication()
                 .withUser("gean")
                 .password(passwordEncoder.encode("senha"))
